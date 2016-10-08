14577d46c7a23cea8cd9002e3086953d827bd0d5
dadahaizhou <824609718@qq.com>
2016-10-08 15:33:05 +0800

    modify collaplayout main2activity

--- app/src/main/java/com/example/zhou/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/zhou/myapplication/Main2Activity.java
@@ -1,10 +1,13 @@
 package com.example.zhou.myapplication;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,6 +32,7 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        initToolBar();
         ButterKnife.bind(this);
         initData();
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -85,5 +89,23 @@ public class Main2Activity extends AppCompatActivity {
                 img = (ImageView) view.findViewById(R.id.m_imageview);
             }
         }
+    }
+
+   void initToolBar(){
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        //使用CollapsingToolbarLayout必须把title设置到CollapsingToolbarLayout上，设置到Toolbar上则不会显示
+        CollapsingToolbarLayout mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout);
+        mCollapsingToolbarLayout.setTitle("CollapsingToolbarLayout");
+        //通过CollapsingToolbarLayout修改字体颜色
+        mCollapsingToolbarLayout.setExpandedTitleColor(Color.WHITE);//设置还没收缩时状态下字体颜色
+        mCollapsingToolbarLayout.setCollapsedTitleTextColor(Color.GREEN);//设置收缩后Toolbar上字体的颜色
     }
 }
